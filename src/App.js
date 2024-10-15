38e92310e6c073ffb8b84961b955cc8d3ff0a84d
celobastos <marceloviniciusbs17@gmail.com>
2024-10-15 09:37:58 -0300

    Style: Ajustes de responsividade no sobre e no cover

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Routing from './routing';  // Import the Routing component
+import Routing from './routing'; 
 
 function App() {
   return (
     <div className="App bg-black h-screen pt-10 text-white flex flex-col ">
-      <Routing />  {/* Use Routing component here */}
+      <Routing /> 
     </div>
   );
 }
